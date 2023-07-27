ce5c7677010352d1974ea4031fd61cf7ccadcfcb
Finbarrs Oketunji <f@finbarrs.eu>
2023-07-27 15:26:13 +0100

    revised
    Signed-off-by: Finbarrs Oketunji <f@finbarrs.eu>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages  # noqa: H301
 from distutils.core import Extension
 
 NAME = "amazon_scrape"
-VERSION = "0.0.9"
+VERSION = "1.0.0"
 REQUIRES = ["beautifulsoup4", "requests"]
 
 # read the contents of your README file
